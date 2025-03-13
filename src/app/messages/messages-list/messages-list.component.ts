05ac0892c247556cb6dbd6a498068858b554e076
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-13 13:23:01 +0300

    Change detection using onPush to detect change where it is absolutely needed Issue with using onPush, services and not using signals Triggering change detection manually using RxJS

---
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { MessagesService } from '../messages.servcies';
+
+@Component({
+  selector: 'app-messages-list',
+  standalone: true,
+  templateUrl: './messages-list.component.html',
+  styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
+})
+export class MessagesListComponent implements OnInit {
+
+  // messages = this.messageSerivce.allMessages
+
+
+  private messageSerivce = inject(MessagesService)
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef)
+  messages: string[] = []
+  ngOnInit(): void {
+    const subscription = this.messageSerivce.messages$.subscribe((messages) => {
+      this.messages = messages
+      this.cdRef.markForCheck()
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe()
+    })
+  }
+
+  get debugOutput() {
+    console.log('[MessagesList] "debugOutput" binding re-evaluated.');
+    return 'MessagesList Component Debug Output';
+  }
+}
