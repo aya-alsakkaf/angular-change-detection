bbe6cd8da5ee674edd138a611e8859e3a4738f0b
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-13 13:29:17 +0300

    change detection using async pipes

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,33 +1,22 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.servcies';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
 
   // messages = this.messageSerivce.allMessages
 
-
   private messageSerivce = inject(MessagesService)
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef)
-  messages: string[] = []
-  ngOnInit(): void {
-    const subscription = this.messageSerivce.messages$.subscribe((messages) => {
-      this.messages = messages
-      this.cdRef.markForCheck()
-    })
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe()
-    })
-  }
+  messages$ = this.messageSerivce.messages$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
